c4296b11b632fd12e941c860f5eaefa569328887
Wells <kyccw55555@hotmail.com>
2017-04-11 14:29:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ def process(patientID, label, rsp=False):
         slices,spacing = resample(slices, patient, [1,1,1])
     slices = resizeImage(triplicate(normalize(slices)),224,224)
 
-    # for slice in slices:
-    #     slice = triplicate(normalize(imresize(slice, (224, 224))))
-    sess = tf.Session()
+    tf.reset_default_graph()
+    # gpu_opts = tf.GPUOptions(per_process_gpu_memory_fraction=0.1)
+    # sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_opts))
+    config = tf.ConfigProto(device_count = {'GPU': 0})
+    sess = tf.Session(config=config)
     imgs = tf.placeholder(tf.float32, [None, 224, 224, 3])
     vgg = vgg16(imgs, 'vgg16_weights.npz', sess)
     bottle_neck = sess.run(vgg.bn, feed_dict={vgg.imgs: slices})
-    slices=None
-    vgg=None
-    imgs=None
+    del patient, slices, sess, imgs, vgg
 
     if label == 1:
         label = np.array([0, 1])
@@ -38,17 +38,17 @@ if __name__ == "__main__":
     patients.sort()
     labels = pd.read_csv('stage1_labels.csv', index_col=0)
     sample_data=[]
-    patients=patients[:3]
+    # patients=patients[:3]
     for num, patientID in enumerate(patients):
         try:
             label = labels.get_value(patientID, 'cancer')
-            rnnInput,label = process(INPUT_FOLDER + patientID, label, rsp=False)
+            rnnInput,label = process(INPUT_FOLDER + patientID, label, rsp=True)
             sample_data.append( (rnnInput,label) )
             print 'Patient',num,'processed.'
 
         except KeyError as err:
             print 'Unlabeled patient:', num, "passed."
 
-    np.save('sample_data.npy', sample_data)
+    np.save('sample_data_rsp.npy', sample_data)
     print 'pre-processed data saved.'
 

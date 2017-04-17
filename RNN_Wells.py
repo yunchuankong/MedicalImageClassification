91783b8cd94c7ced446385a1f80b6dc3145c5c98
Wells <kyccw55555@hotmail.com>
2017-04-16 23:30:12 -0400

    Add files via upload

---
+++ RNN_Wells.py
@@ -0,0 +1,191 @@
+# from __future__ import print_function
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.framework.ops import reset_default_graph
+import RNN_util
+
+reset_default_graph()
+
+# Parameters
+batch_size = 80
+display_step = 500
+# n_embedding = 1000 # ?
+hidden_layer_size = 1000
+input_size = 4096 # n_features
+target_size = 2 # n_classes
+learning_rate = 0.001
+reg_scale = 0.00001
+training_iters = 1000000
+
+def data_summary(data):
+    summary = dict()
+    summary["len"] = len(data)
+    summary["n_slices"] = np.array(list(map(lambda x: len(x[0]), data)))
+    summary["n_features"] = input_size
+    summary["max_seq_len"] = summary["n_slices"].max()
+    summary["cancer"] = np.array(list(map(lambda x: x[1][1], data)))
+    return summary
+
+class DataBatchGenerator(object):
+    """
+    This class generate samples for training and testing (random sampling with a given proportion):
+    - Class 0: normal samples
+    - Class 1: cancer samples
+    NOTICE:
+    We have to pad each sequence to reach 'max_seq_len' for TensorFlow
+    consistency (we cannot feed a numpy array with inconsistent
+    dimensions). The dynamic calculation will then be perform thanks to
+    'seqlen' attribute that records every actual sequence length.
+    """
+
+    def __init__(self, data, summary, test_prop=0.1,batch_size=100):
+        self.images = []
+        self.labels = []
+        self.seqlen = summary["n_slices"]
+        max_seq_len = summary["max_seq_len"]
+        for i in range(summary["len"]):
+            self.images.append(np.concatenate((data[i][0], np.zeros([max_seq_len - self.seqlen[i], input_size]))))
+            # pad each sequence to reach 'max_seq_len'
+            self.labels.append(data[i][1])
+        # Obtain the training set and test set for RNN
+        self.test = np.random.choice(summary["len"], int(round(test_prop * summary["len"])), replace=False)
+        self.train = np.setdiff1d(np.array(range(summary["len"])), self.test)
+        self.batch_size=batch_size
+
+    def next_batch(self,batch_size,batch_id):
+        len_train = len(self.train)
+        n_bins = len_train / batch_size
+        batch_images = []
+        batch_labels = []
+        batch_seqlen = []
+        if batch_id > (n_bins+1):
+            print("batch_id too large!")
+            return None
+        if batch_id == n_bins+1:
+            idx = self.train[batch_size * (batch_id - 1): len_train]
+            for id in idx:
+                batch_images.append(self.images[id])
+                batch_labels.append(self.labels[id])
+                batch_seqlen.append(self.seqlen[id])
+            extr_batch_images, extr_batch_labels, extr_batch_seqlen = \
+                self.next_random_batch(batch_size-len(idx), train_exclude=np.setdiff1d(self.train,idx))
+            batch_images.append(extr_batch_images)
+            batch_labels.append(extr_batch_labels)
+            batch_seqlen.append(extr_batch_seqlen)
+            return batch_images, batch_labels, batch_seqlen
+
+        idx = self.train[ batch_size * (batch_id-1): (batch_size * batch_id - 1) ]
+        for id in idx:
+            batch_images.append(self.images[id])
+            batch_labels.append(self.labels[id])
+            batch_seqlen.append(self.seqlen[id])
+        return batch_images, batch_labels, batch_seqlen
+
+    def next_random_batch(self, batch_size, train_exclude=None):
+        """ Return a batch of data randomly from training set.
+        """
+        if train_exclude:
+            ids = np.setdiff1d(self.train,train_exclude)
+            len_choice = len(ids)
+        else:
+            len_choice = len(self.train)
+        batch_id = np.random.choice(len_choice, batch_size, replace=False)
+        batch_images = []
+        batch_labels = []
+        batch_seqlen = []
+        for i in batch_id:
+            idx = self.train[i]
+            batch_images.append(self.images[idx])
+            batch_labels.append(self.labels[idx])
+            batch_seqlen.append(self.seqlen[idx])
+
+        return batch_images, batch_labels, batch_seqlen
+
+    def get_train(self):
+        """Return the whole training dataset.
+        """
+        train_images = []
+        train_labels = []
+        train_seqlen = []
+        for i in self.train:
+            train_images.append(self.images[i])
+            train_labels.append(self.labels[i])
+            train_seqlen.append(self.seqlen[i])
+
+        return train_images, train_labels, train_seqlen
+
+    def get_test(self):
+        """Return the selected testing dataset.
+        """
+        test_images = []
+        test_labels = []
+        test_seqlen = []
+        for i in self.test:
+            test_images.append(self.images[i])
+            test_labels.append(self.labels[i])
+            test_seqlen.append(self.seqlen[i])
+
+        return test_images, test_labels, test_seqlen
+
+
+# ==========
+#   MODEL
+# ==========
+
+bottle_neck_data = np.load(
+    "/labs/colab/3DDD/kaggle_data/kaggle_processed_data/resampled_transfer_learning_data_new.npy")
+bottle_neck_summary = data_summary(bottle_neck_data)
+dataset = DataBatchGenerator(bottle_neck_data, bottle_neck_summary, 0.10, 100)
+seq_max_len = bottle_neck_summary["max_seq_len"]
+
+y = tf.placeholder(tf.float32, shape=[None, target_size], name='inputs')
+
+# # Models
+# Initializing rnn object
+rnn = RNN_util.LSTM_cell(input_size, hidden_layer_size, target_size)
+# In[6]:
+# Getting all outputs from rnn
+outputs = rnn.get_outputs()
+# Getting final output through indexing after reversing
+last_output = tf.reverse(outputs, [True, False, False])[0, :, :]
+
+# As rnn model output the final layer through Relu activation softmax is
+# used for final output.
+output = tf.nn.softmax(last_output)
+# Computing the Cross Entropy loss
+loss = -tf.reduce_sum(y * tf.log(output))
+regularize = tf.contrib.layers.l2_regularizer(reg_scale)
+params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+reg_term = sum([regularize(param) for param in params])
+loss += reg_term
+
+# Trainning with Adadelta Optimizer
+train_step = tf.train.AdadeltaOptimizer(learning_rate).minimize(loss)
+
+# Calculatio of correct prediction and accuracy
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(output, 1))
+accuracy = (tf.reduce_mean(tf.cast(correct_prediction, tf.float32))) * 100
+
+config = tf.ConfigProto(device_count={'GPU': 0})
+sess = tf.Session(config=config)
+# sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+train_image, train_label, test_seqlen = dataset.get_train()
+test_image, test_label, test_seqlen = dataset.get_test()
+len_train = len(dataset.train)
+n_bins = len_train / batch_size
+for epoch in xrange(training_iters):
+
+    for i in xrange(n_bins+1):
+        batch_x, batch_y, batch_seqlen = dataset.next_batch(batch_size, i+1)
+        sess.run(train_step, feed_dict={rnn._inputs: batch_x, y: batch_y})
+
+    Loss = str(sess.run(loss, feed_dict={rnn._inputs: batch_x, y: batch_x}))
+    Train_accuracy = str(sess.run(accuracy, feed_dict={rnn._inputs: train_image, y: train_label}))
+    Test_accuracy = str(sess.run(accuracy, feed_dict={rnn._inputs: test_image, y: test_label}))
+
+    print("\rIteration: %s Loss: %s Train Accuracy: %s Test Accuracy: %s" %
+          (epoch, Loss, Train_accuracy, Test_accuracy)),
+
+
---
+++ RNN_util.py
@@ -0,0 +1,226 @@
+import tensorflow as tf
+
+
+# # STACKED LSTM class and functions
+
+class LSTM_cell(object):
+
+    """
+    LSTM cell object which takes 3 arguments for initialization.
+    input_size = Input Vector size
+    hidden_layer_size = Hidden layer size
+    target_size = Output vector size
+    """
+
+    def __init__(self, input_size, hidden_layer_size, target_size):
+
+        # Initialization of given values
+        self.input_size = input_size
+        self.hidden_layer_size = hidden_layer_size
+        self.target_size = target_size
+
+        # Weights and Bias for input and hidden tensor
+        self.Wi_l1 = tf.Variable(tf.truncated_normal(
+            [self.input_size, self.hidden_layer_size]))
+        self.Ui_l1 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.bi_l1 = tf.Variable(tf.truncated_normal([self.hidden_layer_size]))
+
+        self.Wf_l1 = tf.Variable(tf.truncated_normal(
+            [self.input_size, self.hidden_layer_size]))
+        self.Uf_l1 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.bf_l1 = tf.Variable(tf.truncated_normal([self.hidden_layer_size]))
+
+        self.Wog_l1 = tf.Variable(tf.truncated_normal(
+            [self.input_size, self.hidden_layer_size]))
+        self.Uog_l1 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.bog_l1 = tf.Variable(
+            tf.truncated_normal([self.hidden_layer_size]))
+
+        self.Wc_l1 = tf.Variable(tf.truncated_normal(
+            [self.input_size, self.hidden_layer_size]))
+        self.Uc_l1 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.bc_l1 = tf.Variable(tf.truncated_normal([self.hidden_layer_size]))
+
+        # Weights for layer 2
+        self.Wi_l2 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.Ui_l2 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.bi_l2 = tf.Variable(tf.truncated_normal([self.hidden_layer_size]))
+
+        self.Wf_l2 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.Uf_l2 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.bf_l2 = tf.Variable(tf.truncated_normal([self.hidden_layer_size]))
+
+        self.Wog_l2 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.Uog_l2 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.bog_l2 = tf.Variable(
+            tf.truncated_normal([self.hidden_layer_size]))
+
+        self.Wc_l2 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.Uc_l2 = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.hidden_layer_size]))
+        self.bc_l2 = tf.Variable(tf.truncated_normal([self.hidden_layer_size]))
+
+        # Weights for output layers
+        self.Wo = tf.Variable(tf.truncated_normal(
+            [self.hidden_layer_size, self.target_size], mean=0, stddev=.1))
+        self.bo = tf.Variable(tf.truncated_normal(
+            [self.target_size], mean=0, stddev=.1))
+
+        # Placeholder for input vector with shape[batch, seq, embeddings]
+        self._inputs = tf.placeholder(tf.float32,
+                                      shape=[None, None, self.input_size],
+                                      name='inputs')
+
+        # Processing inputs to work with scan function
+        self.processed_input = process_batch_input_for_RNN(self._inputs)
+
+        '''
+        Initial hidden state's shape is [1,self.hidden_layer_size]
+        In First time stamp, we are doing dot product with weights to
+        get the shape of [batch_size, self.hidden_layer_size].
+        For this dot product tensorflow use broadcasting. But during
+        Back propagation a low level error occurs.
+        So to solve the problem it was needed to initialize initial
+        hiddden state of size [batch_size, self.hidden_layer_size].
+        So here is a little hack !!!! Getting the same shaped
+        initial hidden state of zeros.
+        '''
+
+        self.initial_hidden = self._inputs[:, 0, :]
+        self.initial_hidden = tf.matmul(
+            self.initial_hidden, tf.zeros([input_size, hidden_layer_size]))
+
+        self.initial_hidden = tf.stack(
+            [self.initial_hidden, self.initial_hidden,
+             self.initial_hidden, self.initial_hidden])
+    # Function for LSTM cell.
+
+    def Lstm(self, previous_hidden_memory_tuple, x):
+        """
+        This function takes previous hidden
+        state and memory tuple with input and
+        outputs current hidden state.
+        """
+
+        previous_hidden_state_l1, c_prev_l1,\
+        previous_hidden_state_l2, c_prev_l2 = tf.unstack(
+            previous_hidden_memory_tuple)
+
+        # Input Gate
+        i_l1 = tf.sigmoid(
+            tf.matmul(x, self.Wi_l1) +
+            tf.matmul(previous_hidden_state_l1, self.Ui_l1) + self.bi_l1
+        )
+
+        # Forget Gate
+        f_l1 = tf.sigmoid(
+            tf.matmul(x, self.Wf_l1) +
+            tf.matmul(previous_hidden_state_l1, self.Uf_l1) + self.bf_l1
+        )
+
+        # Output Gate
+        o_l1 = tf.sigmoid(
+            tf.matmul(x, self.Wog_l1) +
+            tf.matmul(previous_hidden_state_l1, self.Uog_l1) + self.bog_l1
+        )
+
+        # New Memory Cell
+        c__l1 = tf.nn.tanh(
+            tf.matmul(x, self.Wc_l1) +
+            tf.matmul(previous_hidden_state_l1, self.Uc_l1) + self.bc_l1
+        )
+
+        # Final Memory cell
+        c_l1 = f_l1 * c_prev_l1 + i_l1 * c__l1
+
+        # Current Hidden state
+        current_hidden_state_l1 = o_l1 * tf.nn.tanh(c_l1)
+
+        # Input Gate for layer 2
+        i_l2 = tf.sigmoid(
+            tf.matmul(current_hidden_state_l1, self.Wi_l2) +
+            tf.matmul(previous_hidden_state_l2, self.Ui_l2) + self.bi_l2
+        )
+
+        # Forget Gate for layer 2
+        f_l2 = tf.sigmoid(
+            tf.matmul(current_hidden_state_l1, self.Wf_l2) +
+            tf.matmul(previous_hidden_state_l2, self.Uf_l2) + self.bf_l2
+        )
+
+        # Output Gate for layer 2
+        o_l2 = tf.sigmoid(
+            tf.matmul(current_hidden_state_l1, self.Wog_l2) +
+            tf.matmul(previous_hidden_state_l2, self.Uog_l2) + self.bog_l2
+        )
+
+        # New Memory Cell for layer 2
+        c__l2 = tf.nn.tanh(
+            tf.matmul(current_hidden_state_l1, self.Wc_l2) +
+            tf.matmul(previous_hidden_state_l2, self.Uc_l2) + self.bc_l2
+        )
+
+        # Final Memory cell for layer 2
+        c_l2 = f_l2 * c_prev_l2 + i_l2 * c__l2
+
+        # Current Hidden state
+        current_hidden_state_l2 = o_l2 * tf.nn.tanh(c_l2)
+
+        return tf.stack([current_hidden_state_l1,
+                        c_l1, current_hidden_state_l2, c_l2])
+
+    # Function for getting all hidden state.
+    def get_states(self):
+        """
+        Iterates through time/ sequence to get all hidden state
+        """
+
+        # Getting all hidden state throuh time
+        all_hidden_states = tf.scan(self.Lstm,
+                                    self.processed_input,
+                                    initializer=self.initial_hidden,
+                                    name='states')
+        all_hidden_states = all_hidden_states[:, 3, :, :]
+
+        return all_hidden_states
+
+    # Function to get output from a hidden layer
+    def get_output(self, hidden_state):
+        """
+        This function takes hidden state and returns output
+        """
+        output = tf.nn.relu(tf.matmul(hidden_state, self.Wo) + self.bo)
+
+        return output
+
+    # Function for getting all output layers
+    def get_outputs(self):
+        """
+        Iterating through hidden states to get outputs for all timestamp
+        """
+        all_hidden_states = self.get_states()
+
+        all_outputs = tf.map_fn(self.get_output, all_hidden_states)
+
+        return all_outputs
+
+
+def process_batch_input_for_RNN(batch_input):
+    """
+    Process tensor of size [5,3,2] to [3,5,2]
+    """
+    batch_input_ = tf.transpose(batch_input, perm=[2, 0, 1])
+    X = tf.transpose(batch_input_)
+
+    return X
